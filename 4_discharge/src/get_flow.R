2104befd9cc8f2879f9fb8d43a0286dea78d8af2
Oliver <soliver@usgs.gov>
2017-09-27 10:50:38 -0500

    clean up flow names

--- 4_discharge/src/get_flow.R
+++ 4_discharge/src/get_flow.R
@@ -5,6 +5,7 @@ get_flow <- function(summary.flow){
   all_flow <- readNWISdv(summary.flow$siteID,"00060",
                          startDate = min(summary.flow$start),
                          endDate = max(summary.flow$end))
+  all_flow <- renameNWISColumns(all_flow)
   
   return(all_flow)
   
