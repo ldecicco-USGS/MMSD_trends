e474719852adc140bbf5e1fb03874a93976eb882
Oliver <soliver@usgs.gov>
2017-09-27 10:40:42 -0500

    modified summarize_flow to summarize all sites included in Q_1 through Q_4.

--- 3_filter/src/summarize_flow.R
+++ 3_filter/src/summarize_flow.R
@@ -3,7 +3,10 @@ library(dplyr)
 summarize_flow <- function(summary.samples) {
   
   summary.flow <- summary.samples %>%
-    group_by(siteID) %>%
+    select(-siteID) %>%
+    gather(siteID, site_gage, -SITE, -begin, -end, -count, -DA_scale, -adjustment) %>%
+    filter(site_gage != "--") %>%
+    group_by(site_gage) %>%
     summarise(start = min(begin),
               end = max(end))
   
