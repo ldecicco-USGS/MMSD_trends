dea474891376c4ebd47cfc70c3e833c6f2c822c1
Oliver <soliver@usgs.gov>
2017-10-10 11:07:49 -0500

    added rules for cnsoring rules for TP and TSS

--- 2_clean_sample/src/clean_censoring_data.R
+++ 2_clean_sample/src/clean_censoring_data.R
@@ -33,8 +33,25 @@ dat.censored$FC_combined[which(dat.censored$FC_combined<=3)] = 3
 # BOD 5 should be censored at 2 throughout entire record
 
 dat.censored$`rmk_BOD5 (mg/L)`[which(dat.censored$`BOD5 (mg/L)`<=2)] = "<"
-dat.censored$`BOD5 (mg/L)`[which(dat.censored$`BOD5 (mg/L)`<=2)] = "2"
+dat.censored$`BOD5 (mg/L)`[which(dat.censored$`BOD5 (mg/L)`<=2)] = 2
 
-# 
+# TP has multiple levels throughout that aren't clear
+# going to conservatively set the last 2002 level (0.01)
+# as the censoring level pre-2002
+
+dat.censored$`rmk_TP (mg/L)`[which(dat.censored$`TP (mg/L)`<= 0.011)] = "<"
+dat.censored$`TP (mg/L)`[which(dat.censored$`TP (mg/L)`<= 0.011)] = 0.011
+
+# TSS
+# level once censoring is documented = 1
+# Censoring looks like it may jump up to 2 from 1993-2002
+# Censroing at 3 from 1979 through 1992
+
+dat.censored$`rmk_Total Suspended Solids (mg/L)`[which(dat.censored$DATE >= as.POSIXct("1993-01-01") & dat.censored$`Total Suspended Solids (mg/L)` <= 2)] = "<"
+dat.censored$`Total Suspended Solids (mg/L)`[which(dat.censored$DATE >= as.POSIXct("1993-01-01") & dat.censored$`Total Suspended Solids (mg/L)` <= 2)] = 2
+
+dat.censored$`rmk_Total Suspended Solids (mg/L)`[which(dat.censored$DATE < as.POSIXct("1993-01-01") & dat.censored$`Total Suspended Solids (mg/L)` <= 3)] = "<"
+dat.censored$`Total Suspended Solids (mg/L)`[which(dat.censored$DATE < as.POSIXct("1993-01-01") & dat.censored$`Total Suspended Solids (mg/L)` <= 3)] = 3
+
 
 
