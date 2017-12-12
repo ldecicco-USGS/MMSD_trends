cd1b8e1dca73a34d248fbcda3c6acb13869e9751
Laura DeCicco <ldecicco@usgs.gov>
2017-12-12 17:35:17 -0600

    more commands directly in yml

--- 1_get_condor_data/src/plot_data.R
+++ 1_get_condor_data/src/plot_data.R
@@ -1,13 +1,3 @@
-get_master_list <- function(ind_file){
-  master_list <- readRDS(as_data_file(ind_file))
-  return(master_list)
-}
-
-save_master_list <- function(master_list, ind_file){
-  saveRDS(master_list, file = as_data_file(ind_file))
-  gd_put(remote_ind = ind_file, local_source = ind_file)
-  return(master_list)
-}
 
 plot_data <- function(master_list, data_file_ind, file_out){
   eList_lists <- readRDS(as_data_file(data_file_ind))
